b4fb9deb7c873d3b9ce768c81d7b5763323c21e5
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-03-11 19:43:58 +0100

    Update input_processor_gestures.c

--- drivers/input/input_processor_gestures.c
+++ drivers/input/input_processor_gestures.c
@@ -9,7 +9,8 @@
 #include <drivers/input_processor.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/kernel.h>
-
+#include <zephyr/device.h>
+#include <zephyr/pm/device.h>
 #include "input_processor_gestures.h"
 
 #include "touch_detection.h"
